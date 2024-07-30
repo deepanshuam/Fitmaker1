39a2fa5c1d7a9811f9787a5e80db7779621aad8e
deepu123455432 <166910981+deepu123455432@users.noreply.github.com>
2024-07-30 13:00:10 +0530

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar'
 
 const App = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-
-    </div>
+  return (<>
+  
+<Navbar/>
+  </>
   )
 }
 
