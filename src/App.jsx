df98ac554ec63d73295c6f5c90b6ca0217d1f711
deepu123455432 <166910981+deepu123455432@users.noreply.github.com>
2024-08-01 10:59:33 +0530

    dp

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
+import LoginPage from './pages/login/LoginPage'
+// import BodyFocus from './components/bodyfocus/BodyFocus'
 
 const App = () => {
   return (<>
   
 <Navbar/>
+<LoginPage/>
+{/* <BodyFocus/> */}
+<Footer/>
   </>
   )
 }
